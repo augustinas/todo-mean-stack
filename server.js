efe3377a7d3ae10734e295d1025538d5c4d01ea4
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2015-04-30 06:50:16 +0100

    Included logger middleware in the server

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ var port = 1337;
 var connect = require('connect');
 var app = connect();
 
+var logger = function(req, res, next) {
+  console.log(req.method, req.url);
+  next();
+};
+
 var anotherHelloWorld = function(req, res, next) {
   res.setHeader('Content-Type', 'text/plain');
   res.end('This actually does work');
 };
 
-app.use(anotherHelloWorld);
-app.listen(port);
+app.use(logger);
+app.use('/hello', anotherHelloWorld);
 
+app.listen(port);
 console.log('Server running at http://localhost:' + port);
