6218560bc08d02a24482b9c4923c95b180a90541
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2015-04-30 07:43:47 +0100

    Split up server file using MVC structure

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 var port = 1337;
-var express = require('express');
+var express = require('./config/express');
 var app = express();
 
-app.use('/', function(req, res) {
-  res.send('Hello World');
-});
-
 app.listen(port);
+module.exports = app;
 
 console.log('Server running at http://localhost:' + port);
