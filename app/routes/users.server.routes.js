11153f1cbb0800ecd092ba88b4db7cdfaa86c340
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2015-05-01 12:39:25 +0100

    Enabled finding single user in the db

--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -2,4 +2,8 @@ var users = require('../../app/controllers/users.server.controller');
 
 module.exports = function(app) {
   app.route('/users').post(users.create).get(users.list);
+
+  app.route('/users/:userId').get(users.read);
+
+  app.param('userId', users.userByID);
 };
