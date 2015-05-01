26fc2af0c35cc4720dacb54d36814e59b2687480
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2015-05-01 08:36:51 +0100

    Completed MongoDB/mongoose setup

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
-var port = 1337;
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var config = require('./config/config');
+var mongoose = require('./config/mongoose');
 var express = require('./config/express');
+
+var db = mongoose();
 var app = express();
 
-app.listen(port);
+app.listen(config.port);
 module.exports = app;
 
-console.log('Server running at http://localhost:' + port);
+console.log(process.env.NODE_ENV + 'server running at http://localhost:' + config.port);
