407166ca0a15ba4097fe88bfb236ea35d2412660
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2015-05-01 13:01:43 +0100

    Deleting users is now possible

--- server.js
+++ server.js
@@ -10,4 +10,4 @@ var app = express();
 app.listen(config.port);
 module.exports = app;
 
-console.log(process.env.NODE_ENV + 'server running at http://localhost:' + config.port);
+console.log(process.env.NODE_ENV + ' server running at http://localhost:' + config.port);
