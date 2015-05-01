a9a425c0ee80b3beacba546c21d3929060432d5d
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2015-05-01 09:03:05 +0100

    Added new user posting

--- config/express.js
+++ config/express.js
@@ -1,11 +1,20 @@
+var config = require('./config');
 var express = require('express');
+var bodyParser = require('body-parser');
+
 module.exports = function() {
   var app = express();
+
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  app.use(bodyParser.json());
 
   app.set('views', './app/views');
   app.set('view engine', 'ejs');
 
   require('../app/routes/index.server.routes.js')(app);
+  require('../app/routes/users.server.routes.js')(app);
 
   app.use(express.static('./public'));
 
